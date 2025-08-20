67ab9a2d2a847912bd7da2db22eaadd5637edc57
JL7105 <jacemlowe@gmail.com>
2025-08-20 08:21:42 -0500

    WIP: Working with dynamic values & attributes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+
 function App() {
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {reactDescriptions[getRandomInt(2)]} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
